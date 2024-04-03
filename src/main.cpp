4425914a858beb7c4455ab751baca95ab1c1f43a
painnick <painnick@gmail.com>
2024-04-04 01:05:20 +0900

    UPDATE. Support PCA9685

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #define USE_PCA9685_SERVO_EXPANDER
 #define DISABLE_COMPLEX_FUNCTIONS
-#define MAX_EASING_SERVOS 1
+#define MAX_EASING_SERVOS 16
 #define ENABLE_EASE_CUBIC
 #define DEBUG
 
@@ -15,42 +15,55 @@
 
 #define MAIN_TAG "main"
 
-ServoEasing Servo1(PCA9685_DEFAULT_ADDRESS); // If you use more than one PCA9685 you probably must modify MAX_EASING_SERVOS
 #define SERVO1_PIN  0
+#define AnkleLeft_PIN  2
+#define AnkleRight_PIN  3
 
-void setup()
-{
-    ESP_LOGI(MAIN_TAG, "InitializeAndCheckI2CConnection...");
+void getAndAttach16ServosToPCA9685Expander(uint8_t aPCA9685I2CAddress) {
+    ServoEasing *tServoEasingObjectPtr;
 
-    delay(2000);
-
-    auto connectecd = Servo1.InitializeAndCheckI2CConnection(&Serial);
-
-    if (Servo1.attach(SERVO1_PIN, 90, SG90msForServo0Degree, SG90msForServo180Degree) == INVALID_SERVO) {
-        Serial.println(F("Error attaching servo"));
-        while (true) {
-            delay(1000);
+    Serial.print(F("Get ServoEasing objects and attach servos to PCA9685 expander at address=0x"));
+    Serial.println(aPCA9685I2CAddress, HEX);
+    for (uint_fast8_t i = 0; i < PCA9685_MAX_CHANNELS; ++i) {
+        tServoEasingObjectPtr = new ServoEasing(aPCA9685I2CAddress);
+        if (tServoEasingObjectPtr->attach(i, 90, SG90msForServo0Degree, SG90msForServo180Degree) == INVALID_SERVO) {
+            ESP_LOGE(MAIN_TAG, "Not Attached! #%d", i);
         }
     }
+}
+
+void setup() {
+    ESP_LOGI(MAIN_TAG, "InitializeAndCheckI2CConnection...");
+
+    Wire.begin();
+    checkI2CConnection(PCA9685_DEFAULT_ADDRESS, &Serial);
+    getAndAttach16ServosToPCA9685Expander(PCA9685_DEFAULT_ADDRESS);
+
     ESP_LOGI(MAIN_TAG, "Attached!");
     delay(2000);
 }
 
-void loop()
-{
-    ESP_LOGI(MAIN_TAG, "Go to 180");
-    Servo1.startEaseTo(180, 30, START_UPDATE_BY_INTERRUPT);
-    delay(6 * 1000);
+void loop() {
+    setSpeedForAllServos(30);
 
-    ESP_LOGI(MAIN_TAG, "Go to 90");
-    Servo1.startEaseTo(90, 30, START_UPDATE_BY_INTERRUPT);
-    delay(6 * 1000);
+    for (uint_fast8_t i = 0; i <= ServoEasing::sServoArrayMaxIndex; ++i) {
+        ServoEasing::ServoEasingArray[i]->startEaseTo(180);
+    }
+    delay(6000);
 
-    ESP_LOGI(MAIN_TAG, "Go to 0");
-    Servo1.startEaseTo(0, 30, START_UPDATE_BY_INTERRUPT);
-    delay(6 * 1000);
+    for (uint_fast8_t i = 0; i <= ServoEasing::sServoArrayMaxIndex; ++i) {
+        ServoEasing::ServoEasingArray[i]->startEaseTo(90);
+    }
+    delay(6000);
 
-    ESP_LOGI(MAIN_TAG, "Go to 90");
-    Servo1.startEaseTo(90, 30, START_UPDATE_BY_INTERRUPT);
-    delay(6 * 1000);
+    for (uint_fast8_t i = 0; i <= ServoEasing::sServoArrayMaxIndex; ++i) {
+        ServoEasing::ServoEasingArray[i]->startEaseTo(0);
+    }
+    delay(6000);
+
+    for (uint_fast8_t i = 0; i <= ServoEasing::sServoArrayMaxIndex; ++i) {
+        ServoEasing::ServoEasingArray[i]->startEaseTo(90);
+    }
+    delay(6000);
 }
+
